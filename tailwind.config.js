386b1cec64447643536a9beb50ee2047c20af9cc
gsharma03 <gauravsh.9315@gmail.com>
2024-08-08 13:38:48 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        "primary": "#541554",
+        "secondary": "#611F69",
+        "secondary-dark": "#4A154B",
+        "light-gray": "#F4EDE4",
+        "dark-yellow": "#ECB22E",
+        "regular-blue": "#1264A3",
+        "black": "#000000",
+        "white": "#FFFFFF",
+        "gray": "#555555",
+        "blue": "#4285f4",
+      },
+    },
+  },
+  plugins: [],
+}
+
